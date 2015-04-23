e6404f8b7d6278400208f51d7af85d0e41974b7a
Andrew Todd <aqtodd@gmail.com>
2015-04-23 13:56:53 -0700

    removed some repetition in test suite

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Twitter Clone App"
+  end 
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Twitter Clone App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Twitter Clone App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Twitter Clone App"
+    assert_select "title", "About | #{@base_title}"
   end  
 
 end
