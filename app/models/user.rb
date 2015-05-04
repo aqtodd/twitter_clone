d953bf1de761a51396d5d4c46217fd70c92b302e
Andrew Todd <aqtodd@gmail.com>
2015-05-04 10:26:57 -0700

    changed digest and new_token methods in user model to idomatic ruby, updated session controller create method so instance variables are used, updated user login integration test to explicitly check that session and user variable for remember token are equal

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,13 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 8 }  
 
   # returns hash digest of the given string
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # returns a random token
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
   
