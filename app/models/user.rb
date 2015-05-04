b9f01ede1725f5e1ca9e7801ae3633973657614b
Andrew Todd <aqtodd@gmail.com>
2015-05-03 21:39:33 -0700

    implemented login & session cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   
+  attr_accessor :remember_token
+
   # callback to standardize email case
   before_save { self.email.downcase! }
   
@@ -12,5 +14,32 @@ class User < ActiveRecord::Base
   
   has_secure_password  
   validates :password, length: { minimum: 8 }  
+
+  # returns hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
 end
