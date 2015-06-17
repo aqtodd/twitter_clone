0e682fa7c3add7fc8673e5330a26314a03c3c935
Andrew Todd <aqtodd@gmail.com>
2015-06-17 12:38:13 -0700

    added user following functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# users
 User.create!(name:  "Example User",
              email: "example@exampleemail.com",
              password:              "password",
@@ -26,8 +19,17 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..50]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,36 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test 'should follow and unfollow a user' do
+    andrew = users(:andrew)
+    archer = users(:archer)
+    assert_not andrew.following?(archer)
+    andrew.follow(archer)
+    assert andrew.following?(archer)
+    assert archer.followers.include?(andrew)
+    andrew.unfollow(archer)
+    assert_not andrew.following?(archer)
+  end
+
+  test 'feed should have the right posts' do
+    andrew = users(:andrew)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    # posts from followed user
+    lana.microposts.each do |post_following|
+      assert andrew.feed.include?(post_following)
+    end
+
+    # posts from self
+    andrew.microposts.each do |post_self|
+      assert andrew.feed.include?(post_self)
+    end
+
+    # posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not andrew.feed.include?(post_unfollowed)
+    end
+  end
+
 end
